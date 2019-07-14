3ea60abad2f276776a0d143d89b6559aecb42d5c
VimanyuAgg <vimanyu1022007@gmail.com>
2019-07-13 22:08:46 -0400

    add custom test case to show root/index page re-routing

--- Lesson3_submissions/problem_7.py
+++ Lesson3_submissions/problem_7.py
@@ -107,7 +107,10 @@ router.add_handler("/home/about", "about handler")  # add a route
 
 # some lookups with the expected output
 print(router.lookup("/"))  # should print 'root handler'
+
+# Custom TestCase to show that all requests to "www.example.com" & "www.example.com/" are routed to same handler
 print(router.lookup(""))  # should print 'root handler'
+
 print(router.lookup("/home"))  # should print 'not found handler' or None if you did not implement one
 print(router.lookup("/home/about"))  # should print 'about handler'
 print(router.lookup("/home/about/"))  # should print 'about handler' or None if you did not handle trailing slashes
